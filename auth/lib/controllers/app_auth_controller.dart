9d68091c9109cbedb68042567361c0577a893714
aospiridonov <aospiridonov@gmail.com>
2022-11-14 10:18:55 +0300

    add queries for create user with jwt tokens

--- auth/lib/controllers/app_auth_controller.dart
+++ auth/lib/controllers/app_auth_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:auth/models/response_model.dart';
 import 'package:conduit/conduit.dart';
+import 'package:jaguar_jwt/jaguar_jwt.dart';
 
 import '../models/user.dart';
 
@@ -38,19 +41,34 @@ class AppAuthController extends ResourceController {
           body: ResponseModel(
               message: 'Fields password, username and email are required'));
     }
+    final salt = generateRandomSalt();
+    final hashPassword = generatePasswordHash(user.password ?? "", salt);
 
-    final User fetchedUser = User();
-
-    // connect to DB
-    // create user
-    // fetch user
-
-    return Response.ok(ResponseModel(data: {
-      'id': fetchedUser.id,
-      'refreshToken': fetchedUser.refreshToken,
-      'accessToken': fetchedUser.accessToken,
-    }, message: 'Successful registration')
-        .toJson());
+    try {
+      late final int id;
+      await managedContext.transaction((transaction) async {
+        final qCreateUser = Query<User>(transaction)
+          ..values.username = user.username
+          ..values.email = user.email
+          ..values.salt = salt
+          ..values.hashPassword = hashPassword;
+        final createdUser = await qCreateUser.insert();
+        id = createdUser.asMap()['id'];
+        final Map<String, dynamic> tokens = _getTokens(id);
+        final qUpdateTokens = Query<User>(transaction)
+          ..where((user) => user.id).equalTo(id)
+          ..values.accessToken = tokens['access']
+          ..values.refreshToken = tokens['refresh'];
+        await qUpdateTokens.updateOne();
+      });
+      final userData = await managedContext.fetchObjectWithID<User>(id);
+      return Response.ok(ResponseModel(
+        data: userData?.backing.contents,
+        message: 'Successful registration',
+      ));
+    } on QueryException catch (error) {
+      return Response.serverError(body: ResponseModel(message: error.message));
+    }
   }
 
   @Operation.post('refresh')
@@ -69,5 +87,17 @@ class AppAuthController extends ResourceController {
       'accessToken': fetchedUser.accessToken,
     }, message: 'Successful refresh tokens')
         .toJson());
+  }
+
+  Map<String, dynamic> _getTokens(int id) {
+    //TODO remove when release
+    final key = Platform.environment['SECRET_KEY'] ?? 'SECRET_KEY';
+    final accessClaimSet =
+        JwtClaim(maxAge: Duration(hours: 1), otherClaims: {'id': id});
+    final refreshClaimSet = JwtClaim(otherClaims: {'id': id});
+    final tokens = <String, dynamic>{};
+    tokens['access'] = issueJwtHS256(accessClaimSet, key);
+    tokens['refresh'] = issueJwtHS256(refreshClaimSet, key);
+    return tokens;
   }
 }
